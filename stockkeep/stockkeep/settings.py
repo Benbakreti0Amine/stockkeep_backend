a6962494f184f95bf2fd1888fdca079d5ccb76ab
AchrafLarbi <larbiachraf03@gmail.com>
2024-03-17 15:09:06 +0100

    'ajouter_renitialiser_mot_de_passe'

--- stockkeep/stockkeep/settings.py
+++ stockkeep/stockkeep/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,6 +155,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'procodestockkeep@gmail.com'
-EMAIL_HOST_PASSWORD = 'fqru naiv mvea oetb' 
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
