8ffd62e4a29859b40816aae78867c292dcdaea21
AchrafLarbi <larbiachraf03@gmail.com>
2024-03-17 14:59:10 +0100

    'ajouter_renitialiser_mot_de_passe'

--- stockkeep/stockkeep/settings.py
+++ stockkeep/stockkeep/settings.py
@@ -150,3 +150,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'procodestockkeep@gmail.com'
+EMAIL_HOST_PASSWORD = 'fqru naiv mvea oetb' 
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
--- stockkeep/users/serializers.py
+++ stockkeep/users/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from rest_framework.validators import ValidationError
-
+from django.utils.http import urlsafe_base64_decode
+from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from role.models import Role
 
 from .models import User
@@ -34,3 +35,39 @@ class UserSerializer(serializers.ModelSerializer):
 
 
         return user
+
+
+class ResetPasswordEmailSerializer(serializers.Serializer):
+    email = serializers.EmailField(required=True)
+
+class ResetPasswordSerializer(serializers.Serializer):
+    """
+    Reset Password Serializer.
+    """
+
+    new_password = serializers.CharField(write_only=True,min_length=1)
+
+    class Meta:
+        field = ("new_password")
+
+    def validate(self, data):
+        """
+        Verify token and encoded_pk and then set new password.
+        """
+        password = data.get("new_password")
+        print(password)
+        token = self.context.get("kwargs").get("token")
+        encoded_pk = self.context.get("kwargs").get("encoded_pk")
+
+        if token is None or encoded_pk is None:
+            raise serializers.ValidationError("Missing data.")
+
+        pk = urlsafe_base64_decode(encoded_pk).decode()
+        print(pk)
+        user = User.objects.get(pk=pk)
+        if not PasswordResetTokenGenerator().check_token(user, token):
+            raise serializers.ValidationError("The reset token is invalid")
+        print(user)
+        user.set_password(password)
+        user.save()
+        return data
