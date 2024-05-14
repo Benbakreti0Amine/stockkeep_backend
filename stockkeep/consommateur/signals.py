7c5a0ca1f42bc40a5660ebb6b538e08a824a24d8
AchrafLarbi <larbiachraf03@gmail.com>
2024-05-14 23:10:28 +0100

    add statistiques

--- stockkeep/consommateur/signals.py
+++ stockkeep/consommateur/signals.py
@@ -6,30 +6,31 @@ from magasinier.middleware import current_request
 
 @receiver(post_save, sender=BonDeCommandeInterne)
 def send_notification_on_bci(sender, instance, created, **kwargs):
-    if created : 
+    print(created)
+    print(instance.status)
+    if not created:  # Check if the instance is being updated
         if instance.status == 'Created succesfully':
             user = instance.user_id
             consommateur = Consommateur.objects.get(user_ptr=user)
             Notification.objects.get_or_create(
                 recipient=user,
                 message=f"{user.username} from {consommateur.structure}.",
-                role = user.role,
-                titre = "New internal order"
+                role=user.role,
+                titre="New internal order"
             )
-    if not created:  # Check if the instance is being updated
-        if instance.status == 'Consulted by the responsable':
+        elif instance.status == 'Consulted by the responsable':
             user = current_request().user
             Notification.objects.get_or_create(
                 recipient=user,
                 message=f"Mr. {user.username} has validated the internal order ID {instance.id}.",
-                role = user.role,
-                titre = "Internal order validation by Res"
+                role=user.role,
+                titre="Internal order validation by Res"
             )
         elif instance.status == 'Consulted by the director':
             user = current_request().user
             Notification.objects.get_or_create(
                 recipient=user,
                 message=f"Mr. {user.username} has validated the internal order ID {instance.id}.",
-                role = user.role,
-                titre = "Internal order validation"
+                role=user.role,
+                titre="Internal order validation"
             )
